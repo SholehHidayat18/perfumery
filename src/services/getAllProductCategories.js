024ba8c55bb6d530199ee153c1218fc5672d2862
SholehHidayat18 <111202113628@mhs.dinus.ac.id>
2024-11-18 21:30:40 +0700

    first commit

---
+++ src/services/getAllProductCategories.js
@@ -0,0 +1,13 @@
+export default function getAllProductCategories(){
+  return  [   {
+      id: 'FMEN',
+      slug: 'men-parfume',
+      name: `Men's Parfume`
+    },
+    {
+      id: 'FWMEN',
+      slug: 'women-parfume',
+      name: `Women's Parfume`
+    },
+  ]
+}
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,59 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'NAF-1-07-FMEN-001',
+      slug: 'parfume-samsara-men',
+      name: "Parfume Samsara '01",
+      category: `Men's Parfume`,
+      categorySlug: 'men-parfume',
+      price: 1_500_000,
+      stock: 1000,
+      imageUrl: '/assets/images/parfum10.png',
+      description: `Samsara adalah parfum klasik dari Guerlain, pertama kali dirilis pada tahun 1989. Parfum ini dikenal dengan aroma oriental yang memikat, menggabungkan kelembutan bunga seperti melati dan ylang-ylang dengan kehangatan kayu cendana dan vanila.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-002',
+      slug: 'parfume-adidas-men',
+      name: "Parfume Adidas '02",
+      category: `Men's Parfume`,
+      categorySlug: 'men-parfume',
+      price: 159_000,
+      stock: 500,
+      imageUrl: `/assets/images/parfum23.png`,
+      description: `Parfum Adidas dikenal dengan aroma sporty yang segar, cocok untuk aktivitas sehari-hari, olahraga, dan memberikan kesan energik. Dengan kombinasi aroma citrus, herbal, dan kayu, parfum ini memberikan nuansa maskulin dan modern yang tahan hingga 6 jam.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-003',
+      slug: 'parfume-giargio-woman',
+      name: "Parfume Giargio '03",
+      category: `Women's Parfume`,
+      categorySlug: 'women-parfume',
+      price: 500_000,
+      stock: 500,
+      imageUrl: `/assets/images/parfum91.png`,
+      description: `Parfum Giorgio terkenal dengan keharumannya yang elegan dan mewah. Salah satu varian populer adalah Giorgio Armani My Way, yang menawarkan aroma floral dan manis dengan perpaduan orange blossom, tuberose, dan vanila.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-004',
+      slug: 'parfume-private-blue-woman',
+      name: "Parfume Private Blue '04",
+      category: `Women's Parfume`,
+      categorySlug: 'women-parfume',
+      price: 1_300_000,
+      stock: 200,
+      imageUrl: `/assets/images/parfum81.png`,
+      description: `Parfum Private Blue dari Saint Hilaire merupakan wewangian pria dengan karakter aroma aromatic fougère, memberikan kesan segar dan maskulin. Kombinasi utamanya melibatkan lavender, mandarin, leather, dan amber, menciptakan aroma yang elegan sekaligus energik. Parfum ini dirancang untuk pria yang ambisius dan pemberani, cocok untuk aktivitas sehari-hari atau acara khusus.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-LV8-005',
+      slug: 'parfume-boucheron-men',
+      name: "Parfume Boucheron '05",
+      category: `Mens's Parfume`,
+      categorySlug: 'men-parfume',
+      price: 1_100_000,
+      stock: 600,
+      imageUrl: `/assets/images/parfum31.png`,
+      description: `Parfum Boucheron dikenal dengan keharuman mewah yang sering menggabungkan aroma klasik dengan sentuhan modern`
+    },
+  ]
+}
